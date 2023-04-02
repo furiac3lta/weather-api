f23f59bd8c8d9d6b18594fe20092c383f071fb0c
marce <colummarcelo@gmail.com>
2023-04-02 16:11:55 -0300

    add n background

---
+++ src/App.jsx
@@ -0,0 +1,84 @@
+import "./App.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import WeatherCard from "./components/WeatherCard";
+import Loading from "./components/Loading";
+
+function App() {
+  const [latlon, setLatlon] = useState();
+  const [weather, setWeather] = useState();
+  const [search, setSearch] = useState("paris");
+  const [temperature, setTemperature] = useState();
+
+  console.log(weather?.weather[0])
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    setSearch(e.target.input.value.toLowerCase().trim());
+    e.target.input.value = "";
+  };
+  const handleLang = (e) => {
+    setLang("es");
+  };
+  useEffect(() => {
+    const success = (pos) => {
+      const obj = {
+        lat: pos.coords.latitude,
+        lon: pos.coords.longitude,
+      };
+      setLatlon(obj);
+    };
+
+    const error = () => {};
+    navigator.geolocation.getCurrentPosition(success, error);
+  }, [search]);
+ 
+  useEffect(() => {
+    if (latlon) {
+      const apikey = "a6f139938d4bc72a171fbf83d3bcb13b";
+      const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latlon.lat}&lon=${latlon.lon}&appid=${apikey}&q=${search}`;
+      axios
+        .get(url)
+        .then((res) => {
+          const celsius = (res.data.main.temp - 273.15).toFixed(1);
+          const farenheit = ((celsius * 9) / 5 + 32).toFixed(1);
+          setTemperature({ celsius, farenheit });
+          setWeather(res.data);
+        })
+        .catch((err) => console.log(err));
+    }
+  }, [latlon]);
+
+  return (
+    /* ${weather?.weather[0]} */
+    <div
+      className="App"
+     /*  style={{ backgroundImage: "url(/public/assets/background/50d.jpg)" }} */
+      style={{
+        backgroundImage: 
+        `url(/public/assets/${weather?.weather[0].icon}.jpg)`}}
+      /*   style={{
+          backgroundImage: 
+          `url(/src/images/background/${weather?.weather[0].icon}.jpg)`}}  */
+    >
+    
+      {weather ? (
+        <>
+          <div className="contain">
+            {/* {" "} */}
+            <h1 className="title">Wheather app</h1>
+            <form onSubmit={handleSearch} action="">
+              <input id="input" type="text" autoComplete= "off"/>
+              <button>search</button>
+            </form>
+            <WeatherCard weather={weather} temperature={temperature} />
+          </div>
+        </>
+      ) : (
+        <Loading />
+      )}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/WeatherCard.jsx
@@ -0,0 +1,61 @@
+import React, { useState } from "react";
+
+const WeatherCard = ({ weather, temperature }) => {
+  console.log(weather);
+  const [isCelsius, setIsCelsius] = useState(true);
+  const handleChangeT = () => setIsCelsius(!isCelsius);
+console.log(weather?.weather[0].icon)
+  return (
+    <article>
+      <h2>
+        {weather?.name}, {weather?.sys.country}
+      </h2>
+      <section>
+        <header>
+          <img
+           /*  src={`https://openweathermap.org/img/wn/${weather?.weather[0].icon}@4x.png`} */
+           src={`../assets/${weather?.weather[0].icon}.svg`}
+           alt=""
+          />
+        
+        </header>
+        <article>
+          <h1>{}</h1>
+          <h3>
+            <b>Clouds: </b>"{weather?.weather[0].description}"
+          </h3>
+          <ul>
+            <li>
+              <span>
+                <b>Wind Speed: </b>
+              </span>
+              {(weather?.wind.speed * 3.6).toFixed(2)} k/h
+            </li>
+            <li>
+              <span>
+                <b>Clouds: </b>
+              </span>
+              {weather?.clouds.all} %
+            </li>
+            <li>
+              <span>
+                <b>Pressure: </b>
+              </span>
+              {weather?.main.pressure} hPa
+            </li>
+          </ul>
+        </article>
+        <footer>
+          <h2 className="temp">
+            {isCelsius
+              ? `${temperature?.celsius} ºC`
+              : `${temperature?.farenheit} ºF`}
+          </h2>
+          <button onClick={handleChangeT}>Change to {isCelsius ? `ºF` : `ºC`}</button>
+        </footer>
+      </section>
+    </article>
+  );
+};
+
+export default WeatherCard;
